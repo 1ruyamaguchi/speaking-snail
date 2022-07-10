65a86dc3fe67af7364178cb57a71d7dba69b6acf
1ruyamaguchi <1ru.yamaguchi@gmail.com>
2022-07-10 13:56:04 +0900

    init

---
+++ src/main/java/com/example/speakingsnail/SpeakingsnailApplication.java
@@ -0,0 +1,13 @@
+package com.example.speakingsnail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakingsnailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeakingsnailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/speakingsnail/SpeakingsnailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.speakingsnail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakingsnailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
