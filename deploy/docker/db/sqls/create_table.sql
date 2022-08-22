f68ddd187e95d01d25f1142289db6fef31b5aecd
1ruyamaguchi <1ru.yamaguchi@gmail.com>
2022-08-22 23:53:25 +0900

    データベース周りWIP

---
+++ deploy/docker/db/sqls/create_table.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE test;
+USE test;
+CREATE TABLE test_table(
+    id int AUTO_INCREMENT NOT NULL,
+    name VARCHAR(10) NOT NULL,
+    PRIMARY KEY (id)
+)
--- deploy/docker/docker-build.sh
+++ deploy/docker/docker-build.sh
@@ -1,7 +1,18 @@
-IMAGE_NAME="speaking-snail"
+###########################################
+### Javaアプリケーションのイメージ
+###########################################
+JAVA_IMAGE_NAME="speaking-snail"
 
 # jarファイルをコピー
-cp ../../target/speakingsnail-0.0.1-SNAPSHOT.jar ./jar
+cp ../../target/speakingsnail-0.0.1-SNAPSHOT.jar ./java/jar
 
 # イメージをビルド
-docker build -t ${IMAGE_NAME} .
+docker build -t ${JAVA_IMAGE_NAME} ./java
+
+###########################################
+### データベースのイメージ
+###########################################
+DB_IMAGE_NAME="snail-db"
+
+# イメージをビルド
+docker build -t ${DB_IMAGE_NAME} ./db
