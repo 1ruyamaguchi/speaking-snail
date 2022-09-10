d5295a8cfbed6ae371eb8f4729fec26e9c493143
1ruyamaguchi <1ru.yamaguchi@gmail.com>
2022-09-10 12:41:29 +0900

    enumを使うところまで切り戻し

--- deploy/docker/docker-build.sh
+++ deploy/docker/docker-build.sh
@@ -9,10 +9,11 @@ cp ../../target/speakingsnail-0.0.1-SNAPSHOT.jar ./java/jar
 # イメージをビルド
 docker build -t ${JAVA_IMAGE_NAME} ./java
 
-###########################################
-### データベースのイメージ
-###########################################
-DB_IMAGE_NAME="snail-db"
+# 日本語が変換されないバグがあるので一旦保留
+# ###########################################
+# ### データベースのイメージ
+# ###########################################
+# DB_IMAGE_NAME="snail-db"
 
-# イメージをビルド
-docker build -t ${DB_IMAGE_NAME} ./db
+# # イメージをビルド
+# docker build -t ${DB_IMAGE_NAME} ./db
